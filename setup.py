5864a283f6c27e36a165979cb9690944afe0b447
PhoxSpark <luisgracia@phoxspark.com>
2019-07-02 15:07:28 +0200

    First release 0.0.3 of PythomProject downloader.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytomproject",
+    version="0.0.3",
+    author="Luis Gracia",
+    author_email="luisgracia@phoxspark.com",
+    description="BSC Python Practice for Bioinformatics.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PhoxSpark/pytom-project",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

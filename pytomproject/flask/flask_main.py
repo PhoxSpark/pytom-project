a9849ec698237822553709025849fd7089235ae0
PhoxSpark <luisgracia@phoxspark.com>
2019-07-03 16:25:41 +0200

    Full flask implementation with bootstrap and a description added to the WebApp

--- pytomproject/flask/flask_main.py
+++ pytomproject/flask/flask_main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title="Home")
 
 app.run(debug=True)

4b195ca89ecc815fd75a6a21dbb4a4e54af7061b
PhoxSpark <luisgracia@phoxspark.com>
2019-07-23 16:49:55 +0200

    Apply changes to init file.

--- pytomproject/__init__.py
+++ pytomproject/__init__.py
@@ -1,20 +1,25 @@
-import os, logging, sys
+"""
+Initialization of pytomproject package.
+"""
+import os
+import logging
+import sys
+from datetime import datetime
 from .general_functions import split
 from .flask_pack import run_flask
-from datetime import datetime
 
 #------------------------------------------------------------------------------
-# Take arguments
+# Take ARGUMENTs
 #------------------------------------------------------------------------------
-argument = []
-flags = []
+ARGUMENT = []
+FLAGS = []
 
 for argv in sys.argv:
-    argument.append(argv)
+    ARGUMENT.append(argv)
 
-for args in argument:
-    if(args.startswith('-')):
-        flags = split(args)
+for args in ARGUMENT:
+    if args.startswith('-'):
+        FLAGS = split(args)
 
 #------------------------------------------------------------------------------
 # Initialize Logger
@@ -23,34 +28,40 @@ for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
 
 # Look for flags
-if('L' in flags):
+if 'L' in FLAGS:
 
-    if(not os.path.exists("log/")):
+    if not os.path.exists("log/"):
         os.makedirs("log/")
 
-    now = datetime.now()
-    logging.basicConfig(filename='log/pytom-log-%s.log' % now.strftime("%d_%m_%Y-%H:%M:%S"), level=logging.DEBUG, format="%(asctime)s-[%(levelname)s]: %(message)s")
+    NOW = datetime.now()
+    logging.basicConfig(filename='log/pytom-log-%s.log' % NOW.strftime("%d_%m_%Y-%H:%M:%S"), \
+    level=logging.DEBUG, format="%(asctime)s-[%(levelname)s]: %(message)s")
 
-if('l' in flags):
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s-[%(levelname)s]: %(message)s")
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.INFO)
-    handler.setFormatter(formatter)
-    root.addHandler(handler)
+if 'l' in FLAGS:
+    ROOT = logging.getLogger()
+    ROOT.setLevel(logging.DEBUG)
+    FORMATTER = logging.Formatter("%(asctime)s-[%(levelname)s]: %(message)s")
+    HANDLER = logging.StreamHandler(sys.stdout)
+    HANDLER.setLevel(logging.INFO)
+    HANDLER.setFormatter(FORMATTER)
+    ROOT.addHandler(HANDLER)
 
-if("file-logging" in argument or "console-logging" in argument):
-    logging.info("Test Information message: This messages will inform about what the program is doing.")
-    logging.warning("Test warning message: This messages will advise you about something.")
-    logging.error("Test error message: This messages will inform about problems while executing the program.")
-    logging.critical("Test critical message: This messages will inform about major problems in the program that may interrumpt it.")
-    logging.debug("Test debug message: This messages are for the programmers, it will display some useful information.")
+if("file-logging" in ARGUMENT or "console-logging" in ARGUMENT):
+    logging.info("Test Information message: This messages will inform about what \
+    the program is doing.")
+    logging.warning("Test warning message: This messages will advise you about \
+    something.")
+    logging.error("Test error message: This messages will inform about problems \
+    while executing the program.")
+    logging.critical("Test critical message: This messages will inform about major \
+    problems in the program that may interrumpt it.")
+    logging.debug("Test debug message: This messages are for the programmers, \
+    it will display some useful information.")
 
 #------------------------------------------------------------------------------
 # Helper
 #------------------------------------------------------------------------------
-if('h' in flags):
+if 'h' in FLAGS:
 
     with open("README.md", 'r') as fin:
         print(fin.read())
